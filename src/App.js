e3d80743301decfc3d19d66b7009815a91e30109
Juan <juanpablozabala21@gmail.com>
2020-06-03 18:43:08 -0400

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function Helloworld (){
   return (
-    <div>Hello World</div>
+    <div id="hello"> Hello World </div>
   );
 }
+
+// const App = () => <div>This is my component: <Helloworld/> </div>
+
+class App extends React.Component {
+  render(){
+    return <div>This is my component  <Helloworld/> </div>
+  }
+}
+
+// function App() {
+//   return (
+//     <div>Este es mi componente: <Helloworld/></div>
+//   );
+// }
 
 export default App;
