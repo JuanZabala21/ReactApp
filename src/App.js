76266991e8d29ec97dbfd6f0ca8d8e2c3af3110c
Juan <juanpablozabala21@gmail.com>
2020-06-05 17:36:51 -0400

    Practice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import './App.css';
 
-function Helloworld (props){
-  console.log(props)
-  return (
-    <div id="hello"> 
-    <h3>{props.subtitle}</h3>
-    {props.mytest} 
-    </div>
-  );
+
+// function Helloworld (props){
+//   console.log(props)
+//   return (
+//     <div id="hello"> 
+//     <h3>{props.subtitle}</h3>
+//     {props.mytest} 
+//     </div>
+//   );
+// }
+
+class Helloworld extends React.Component {
+
+  state = {
+    show: true
+  }
+
+toggleShow = () => {
+this.setState({show: !this.state.show})
 }
+
+  render(){
+    if(this.state.show){
+    return(
+      <div id="hello"> 
+      <h3>{this.props.subtitle}</h3>
+      {this.props.mytest} 
+      <button onClick={this.toggleShow}>Toggle Show</button>
+      </div>
+    )   
+   }else{
+    return <h1> There are not elements 
+      <button onClick={this.toggleShow}>Toggle Show</button>
+    </h1>
+   }
+  }
+ }
 
 // const App = () => <div>This is my component: <Helloworld/> </div>
 
