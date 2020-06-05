b1057a1ae396106c89b2df31205ce2803977d31e
Juan <juanpablozabala21@gmail.com>
2020-06-05 18:28:28 -0400

    Tasks

--- src/App.js
+++ src/App.js
@@ -1,61 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import tasks from './example/task.json';
+
+import Tasks from './components/Tasks';
 
 
-// function Helloworld (props){
-//   console.log(props)
-//   return (
-//     <div id="hello"> 
-//     <h3>{props.subtitle}</h3>
-//     {props.mytest} 
-//     </div>
-//   );
-// }
-
-class Helloworld extends React.Component {
-
+class App extends Component {
+  
   state = {
-    show: true
+    tasks: tasks
   }
 
-toggleShow = () => {
-this.setState({show: !this.state.show})
-}
-
-  render(){
-    if(this.state.show){
-    return(
-      <div id="hello"> 
-      <h3>{this.props.subtitle}</h3>
-      {this.props.mytest} 
-      <button onClick={this.toggleShow}>Toggle Show</button>
-      </div>
-    )   
-   }else{
-    return <h1> There are not elements 
-      <button onClick={this.toggleShow}>Toggle Show</button>
-    </h1>
-   }
-  }
- }
-
-// const App = () => <div>This is my component: <Helloworld/> </div>
-
-class App extends React.Component {
-  render(){
+  render() {
     return <div>
-      This is my component 
-       <Helloworld mytest="hello fast" subtitle="Lorem ipsum"/>
-       <Helloworld mytest="Hola Mundo" subtitle="Componente 2"/>  
-       <Helloworld mytest="Epale" subtitle="Practicando"/> 
-       </div>
+    <Tasks tasks={this.state.tasks}/>
+    </div> 
+   
   }
 }
-
-// function App() {
-//   return (
-//     <div>Este es mi componente: <Helloworld/></div>
-//   );
-// }
-
 export default App;
