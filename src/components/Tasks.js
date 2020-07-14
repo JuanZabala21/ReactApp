f027320fbe6a96c83b89ac37d020afb85ca2cca1
Juan <juanpablozabala21@gmail.com>
2020-07-14 17:39:45 -0400

    PropTypes

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,14 @@
 import React, { Component} from 'react'; 
 import Task from './Task'; 
+import PropTypes from 'prop-types';
 
 class Tasks extends Component {
     render() {
         return this.props.tasks.map(task => <Task task={task} key={task.id}/>);
     }
+}
+Task.propTypes = {
+    tasks: PropTypes.object.isRequired
 }
 
 export default Tasks; 
